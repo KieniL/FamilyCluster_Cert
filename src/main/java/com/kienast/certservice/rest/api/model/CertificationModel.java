8b75dcf7eb0bf4f375743dc334d51244d0419b6f
Lukas Kienast <lukaskienast0@gmail.com>
2021-06-20 07:12:11 +0200

    Updated openapi

--- src/main/java/com/kienast/certservice/rest/api/model/CertificationModel.java
+++ src/main/java/com/kienast/certservice/rest/api/model/CertificationModel.java
@@ -1,14 +1,13 @@
 package com.kienast.certservice.rest.api.model;
 
-import java.time.LocalDate;
 import java.util.Objects;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDate;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 
 /**
  * CertificationModel
