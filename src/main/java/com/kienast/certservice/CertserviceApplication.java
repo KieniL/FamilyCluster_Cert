733d5a47a0b6cfb491338e2c22793f5f53e21c20
Lukas Kienast <lukaskienast0@gmail.com>
2021-07-22 15:10:38 +0200

    Added jwt verification

--- src/main/java/com/kienast/certservice/CertserviceApplication.java
+++ src/main/java/com/kienast/certservice/CertserviceApplication.java
@@ -2,10 +2,17 @@ package com.kienast.certservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class CertserviceApplication {
 
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CertserviceApplication.class, args);
 	}
--- src/main/java/com/kienast/certservice/controller/CertificationController.java
+++ src/main/java/com/kienast/certservice/controller/CertificationController.java
@@ -1,11 +1,14 @@
 package com.kienast.certservice.controller;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import com.kienast.certservice.exception.BadRequestException;
+import com.kienast.certservice.exception.NotAuthorizedException;
+import com.kienast.certservice.model.TokenVerificationResponse;
 import com.kienast.certservice.rest.api.CertApi;
 import com.kienast.certservice.rest.api.model.CertificationModel;
 import com.kienast.certservice.service.CertificationService;
@@ -15,8 +18,14 @@ import org.apache.logging.log4j.Logger;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 
@@ -30,12 +39,39 @@ public class CertificationController implements CertApi {
 	@Value("${logging.level.com.kienast.certservice}")
 	private String loglevel;
 
+	// Used for WebTemplate
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
+	@Value("${authURL}")
+	private String authURL;
+
 	@Override
 	public ResponseEntity<CertificationModel> addCertifaction(String JWT, String xRequestID, String SOURCE_IP,
 			@Valid CertificationModel certificationModel) {
 
 		initializeLogInfo(xRequestID, SOURCE_IP, "1");
 		logger.info("Got Request (Add Certification) for " + certificationModel.getDescription());
+
+		try {
+			TokenVerificationResponse tokenResponse = new TokenVerificationResponse();
+
+			logger.info("Call Authentication Microservice for JWT Verification");
+			tokenResponse = webClientBuilder.build().post() // RequestMethod
+					.uri(authURL + "/jwt").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).header("JWT", JWT)
+					.header("X-Request-ID", xRequestID).header("SOURCE_IP", SOURCE_IP).retrieve() // run command
+					.onStatus(HttpStatus::is4xxClientError, response -> {
+						return Mono.error(new NotAuthorizedException(String.format("Failed JWT Verification")));
+					}).bodyToMono(TokenVerificationResponse.class) // convert Response
+					.block(); // do as Synchronous call
+
+			initializeLogInfo(xRequestID, SOURCE_IP, tokenResponse.getUserId());
+			logger.info("Added userId to log");
+		} catch (Exception e) {
+
+			logger.error("Error on verifiying jwt");
+			throw new BadRequestException("Verification Failure");
+		}
 
 		try {
 
@@ -59,22 +95,64 @@ public class CertificationController implements CertApi {
 	@Override
 	public ResponseEntity<CertificationModel> getCertification(String shortname, String JWT, String xRequestID,
 			String SOURCE_IP) {
+		CertificationModel certification = null;
 		initializeLogInfo(xRequestID, SOURCE_IP, "1");
 		logger.info("Got Request (Get Certification) for " + shortname);
 
-		CertificationModel certification = certificationService.getCertificationByShortname(shortname);
-		logger.info("Successfully loaded certification " + shortname);
+		try {
+			TokenVerificationResponse tokenResponse = new TokenVerificationResponse();
+
+			logger.info("Call Authentication Microservice for JWT Verification");
+			tokenResponse = webClientBuilder.build().post() // RequestMethod
+					.uri(authURL + "/jwt").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).header("JWT", JWT)
+					.header("X-Request-ID", xRequestID).header("SOURCE_IP", SOURCE_IP).retrieve() // run command
+					.onStatus(HttpStatus::is4xxClientError, response -> {
+						return Mono.error(new NotAuthorizedException(String.format("Failed JWT Verification")));
+					}).bodyToMono(TokenVerificationResponse.class) // convert Response
+					.block(); // do as Synchronous call
+
+			initializeLogInfo(xRequestID, SOURCE_IP, tokenResponse.getUserId());
+			logger.info("Added userId to log");
+			certification = certificationService.getCertificationByShortname(shortname);
+			logger.info("Successfully loaded certification " + shortname);
+
+		} catch (Exception e) {
+
+			logger.error("Error on verifiying jwt");
+		}
 
 		return ResponseEntity.ok(certification);
 	}
 
 	@Override
 	public ResponseEntity<List<CertificationModel>> getCertifications(String JWT, String xRequestID, String SOURCE_IP) {
+		List<CertificationModel> certifications = new ArrayList<>();
+
 		initializeLogInfo(xRequestID, SOURCE_IP, "1");
 		logger.info("Got Request (Get Certifications)");
 
-		List<CertificationModel> certifications = certificationService.getCertifications();
-		logger.info("Successfully loaded all Certifications");
+		try {
+			TokenVerificationResponse tokenResponse = new TokenVerificationResponse();
+
+			logger.info("Call Authentication Microservice for JWT Verification");
+			tokenResponse = webClientBuilder.build().post() // RequestMethod
+					.uri(authURL + "/jwt").header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).header("JWT", JWT)
+					.header("X-Request-ID", xRequestID).header("SOURCE_IP", SOURCE_IP).retrieve() // run command
+					.onStatus(HttpStatus::is4xxClientError, response -> {
+						return Mono.error(new NotAuthorizedException(String.format("Failed JWT Verification")));
+					}).bodyToMono(TokenVerificationResponse.class) // convert Response
+					.block(); // do as Synchronous call
+
+			initializeLogInfo(xRequestID, SOURCE_IP, tokenResponse.getUserId());
+			logger.info("Added userId to log");
+
+			certifications = certificationService.getCertifications();
+			logger.info("Successfully loaded all Certifications");
+
+		} catch (Exception e) {
+
+			logger.error("Error on verifiying jwt");
+		}
 
 		return ResponseEntity.ok(certifications);
 	}
